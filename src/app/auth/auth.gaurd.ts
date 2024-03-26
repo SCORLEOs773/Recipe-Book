e3dc3a6379cdf35c9956002a77c7ce0b35300702
SCORLEOs773 <mykeyman777@gmail.com>
2024-03-26 15:28:04 +0530

    Auth Gaurd Updates

--- src/app/auth/auth.gaurd.ts
+++ src/app/auth/auth.gaurd.ts
@@ -1,4 +1,4 @@
-import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from "@angular/router";
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from "@angular/router";
 import { Observable } from "rxjs";
 import { map } from "rxjs/operators";
 import { AuthService } from "./auth.service";
@@ -7,11 +7,16 @@ import { Injectable } from "@angular/core";
 @Injectable({providedIn: 'root'})
 export class AuthGaurd implements CanActivate {
 
-  constructor(private authService: AuthService) {}
+  constructor(private authService: AuthService, private router: Router) {}
 
-  canActivate(route: ActivatedRouteSnapshot, router: RouterStateSnapshot): boolean | Promise<boolean> | Observable<boolean> {
+  canActivate(route: ActivatedRouteSnapshot, router: RouterStateSnapshot): boolean | Promise<boolean> | Observable<boolean | UrlTree> {
     return this.authService.user.pipe(map(user => {
-      return !!user;
+      const isAuth = !!user
+      if(isAuth)
+      {
+        return true;
+      }
+      return this.router.createUrlTree(['/auth']);
     }))
   }
 }
