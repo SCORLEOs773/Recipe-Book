cfe05d5dea47d613ac33bd538ca5c943fe595791
SCORLEOs773 <mykeyman777@gmail.com>
2024-04-03 19:26:57 +0530

    Search Bar Fully Functional

--- src/app/recipes/recipe.model.ts
+++ src/app/recipes/recipe.model.ts
@@ -1,20 +1,23 @@
 import { Ingredient } from '../shared/ingredient.model';
 
 export class Recipe {
+  public id: string;
   public name: string;
   public description: string;
   public imagePath: string;
   public ingredients: Ingredient[];
 
   constructor(
+    id: string,
     name: string,
     description: string,
     imagePath: string,
     ingredients: Ingredient[]
   ) {
+    this.id = id;
     this.name = name;
     this.description = description;
     this.imagePath = imagePath;
     this.ingredients = ingredients;
   }
-} 
+}
